b20f3c8e3d163353531fc26c07e48693226e86ca
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2026-03-11 12:15:03 -0300

    primeiro commit

---
+++ nodefaturas/index.js
@@ -0,0 +1,11 @@
+//Rotas da aplicação
+if (url === "/") {
+  res.end("Rota raiz");
+} else if (url === "/contato") {
+  res.end("Pagina de contato");
+} else if (url === "/sobre") {
+  res.end("Pagina sobre");
+} else {
+  res.writeHead(404); //rota nao cadastrada
+  res.end("Rota nao cadastrada");
+}
